92bb31ae7c22636379555690330e72f84fc3564a
DylanMatthewHall <dylanmatthewhall@outlook.com>
2025-09-30 19:51:41 -0500

    removed apptest

--- streamtrack/src/main/java/com/dylan/Application.java
+++ streamtrack/src/main/java/com/dylan/Application.java
@@ -1,7 +1,19 @@
 package com.dylan;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+    private List<StreamingService> services;
+    private Scanner scanner;
+
+    // Constructor
+    public Application() {
+
+    }
+
+    // Core Methods
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
